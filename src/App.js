68a2c2bb5be1bf55d444ce0d833fb4c2fb669d38
Arthur Malheiros <arthurmalheiros@teknisa.com>
2020-08-10 23:28:26 -0300

    funcao para remover repositorio inclusa

--- src/App.js
+++ src/App.js
@@ -1,30 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header"
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    // setRepositories([...repositories, `Repositório ${Date.now()}`])
+
+    const response = await api.post('repositories', {
+      title: `Repositório ${Date.now()}`,
+      techs: "Arthur Malheiros",
+      url: "arthur.com.br"
+    })
+
+    const repository = (await response).data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`/repositories/${id}/`);
+
+    const repositoryIndex = repositories.findIndex(
+      (repository) => repository.id === id
+    );
+
+    repositories.splice(repositoryIndex, 1);
+
+    setRepositories(response.data);
   }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+    <>
+      <Header title="Repositories">
+        <ul data-testid="repository-list">
+          {repositories.map((repository) => (
+            <li key={repository.id}>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>-</button>
+            </li>
+          ))}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+        </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
+        <button onClick={handleAddRepository}>Adicionar</button>
+      </Header>
+    </>
   );
 }
 
